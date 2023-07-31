d00d0f77fc89ffaa4931d9676ba1d3654754cfca
axinger <liu_weixing@qq.com>
2023-07-31 10:43:02 +0800

    update

---
+++ src/main/java/com/github/axinger/AxingerHadoopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.axinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxingerHadoopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxingerHadoopDemoApplication.class, args);
+    }
+
+}
